831161ea04c48b179bc7dd559a13cc5398e14182
Artin Mirzayans <artin.mirzayans@gmail.com>
2022-10-05 18:42:02 -0700

    oopsies!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV["RENDER"].present?
 
-  Compress CSS using a preprocessor.
+  # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
